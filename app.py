503e0848fa957afe16d090e2337e4b8d22444a86
Jorge Ivan Garcia Piña <68978leo@universidaddeleon.edu.mx>
2026-03-12 08:49:29 -0600

    maestros

--- app.py
+++ app.py
@@ -2,17 +2,46 @@ from flask  import Flask, render_template,request, redirect, url_for
 from flask import flash
 from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
-from models import db, Alumnos
+from models import db, Alumnos, Maestros # Agregado Maestros
 import forms  
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 db.init_app(app)
 csrf = CSRFProtect(app)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    create_alumno = forms.UserForm(request.form)
+    alumno = Alumnos.query.all()
+    return render_template("index.html", form=create_alumno, alumno=alumno)
+
+@app.route("/maestros",methods=["GET","POST"])
+def maestros():
+    mat=0
+    nom=''
+    ape=''
+    esp=''
+    email=''
+    maestros_clas=forms.MaestroForm(request.form)
+    if request.method=='POST':
+        mat=maestros_clas.matricula.data
+        nom=maestros_clas.nombre.data
+        ape=maestros_clas.apellidos.data
+        esp=maestros_clas.especialidad.data
+        email=maestros_clas.correo.data
+        
+        # Guardar en la base de datos igual que Alumnos
+        nuevo_maestro = Maestros(matricula=mat, nombre=nom, apellidos=ape, 
+                                 especialidad=esp, email=email)
+        db.session.add(nuevo_maestro)
+        db.session.commit()
+    
+    # Select para mostrar en la tabla de maestros.html
+    lista_maestros = Maestros.query.all()
+    
+    return render_template('maestros.html', form=maestros_clas, maestros=lista_maestros,
+                           mat=mat, nom=nom, ape=ape, esp=esp, email=email)
 
 @app.route("/usuarios",methods=["GET","POST"])
 def usuario():
@@ -39,5 +68,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run()
-
-      
